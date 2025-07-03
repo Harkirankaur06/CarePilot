0febb22a3383ea91d0ebe77330f39a093aed2cff
Harkiran Kaur <98973889+Harkirankaur06@users.noreply.github.com>
2025-07-03 18:25:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import send_from_directory
 import bo
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -30,4 +31,5 @@ def chat():
     return jsonify({"reply": response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
